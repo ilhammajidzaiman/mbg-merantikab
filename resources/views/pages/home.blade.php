71f3aa8183a7b540513e5d6a1a502a788cc6f0aa
ilhammajidzaiman <ilhammajidzaiman07@gmail.com>
2025-10-15 14:42:09 +0700

    FIX: tampilkan data artikel dari model

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -200,14 +200,13 @@
                         </h3>
                     </div>
                 </div>
-                @if ($berita)
-                    {{-- @if ($berita->isNotEmpty()) --}}
+                @if ($berita->isNotEmpty())
                     @foreach ($berita as $item)
                         <div class="col-span-full md:col-span-4">
                             <div class="">
                                 <div class="flex flex-col space-y-2">
                                     <div class="aspect-video overflow-hidden rounded-lg"><img
-                                            src="{{ $item->file ?? null }}"
+                                            src="{{ Storage::url($item->file) }}"
                                             class="bg-slate-200 w-full h-full object-cover transition duration-300 ease-in-out hover:scale-110">
                                     </div>
                                     <h1 class="line-clamp-2 mb-4">
