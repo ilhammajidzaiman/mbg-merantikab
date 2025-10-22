0a15e9a1552b5deb10b353f64c109c530b39665a
ilhammajidzaiman <ilhammajidzaiman07@gmail.com>
2025-10-22 10:01:49 +0700

    FIX: perbaikan thumbnail pada galeri

--- resources/views/pages/galery/index.blade.php
+++ resources/views/pages/galery/index.blade.php
@@ -19,7 +19,7 @@
                                 <div class="bg-white rounded-lg shadow-xs gap-4 cursor-pointer"
                                     @click="selected = { file: '{{ Storage::url($item->file ?? null) }}', description: '{{ $item->description ?? null }}' }; open = true;">
                                     <div class="aspect-video overflow-hidden rounded-lg">
-                                        <img src="{{ Storage::url($item->file ?? null) }}"
+                                        <img src="{{ Storage::url('thumbnail/' . $item->file ?? null) }}"
                                             class="bg-slate-200 w-full h-full object-cover transition duration-300 ease-in-out hover:scale-110">
                                     </div>
                                 </div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -264,7 +264,7 @@
                                 <div class="bg-white rounded-lg shadow-xs gap-4 cursor-pointer"
                                     @click="selected = { file: '{{ Storage::url($item->file) }}', description: '{{ $item->description ?? null }}' }; open = true;">
                                     <div class="aspect-video overflow-hidden rounded-lg">
-                                        <img src="{{ Storage::url($item->file) }}"
+                                        <img src="{{ Storage::url('thumbnail/' . $item->file ?? null) }}"
                                             class="bg-slate-200 w-full h-full object-cover transition duration-300 ease-in-out hover:scale-110">
                                     </div>
                                 </div>
